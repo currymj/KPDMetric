a65a1babfa4beedcd5f77be740a5032dc28425e1
Michael J. Curry <currymj@umd.edu>
2017-10-29 09:47:24 -0400

    python 3 fixes

--- python/BayesOpt.py
+++ python/BayesOpt.py
@@ -1,10 +1,12 @@
-from GPyOpt.methods import BayesianOptimization
-import numpy as np
-import subprocess
-import pickle
+from __future__ import absolute_import, division, print_function, unicode_literals
 import os
+import pickle
+import subprocess
 
-print "Starting..."
+import numpy as np
+from GPyOpt.methods import BayesianOptimization
+
+print("Starting...")
 
 TRAJECTORIES = 32
 PAIRS_0 = 250
@@ -14,22 +16,28 @@ E_A = 1
 
 jargs = []
 
+
 def l2f(a):
     s = ""
     for i in a:
         s += str(i)
     return s
 
+
 def f(Xl):
     global X
     global Y
-    R = np.empty([0,1])
+    R = np.empty([0, 1])
     for XI in range(len(Xl)):
         arg = Xl[XI]
         X = np.append(X, [arg], axis=0)
-        MT = 0
+        MT = 0 
         for i in range(TRAJECTORIES):
-            I = ["java","-jar","Simulation.jar"]
+            I = [
+                "java",
+                "-Djava.library.path=/Users/curry/Applications/IBM/ILOG/CPLEX_Studio1271/cplex/bin/x86-64_osx",
+                "-Xmx8g", "-jar", "Simulation1.jar"
+            ]
             I.append(str(arg[0]))
             for j in jargs:
                 I.append(str(j))
@@ -37,44 +45,50 @@ def f(Xl):
             I.append(str(ALTS_0))
             I.append(str(E_P))
             I.append(str(E_A))
-            print "Running the simulator"
-            out = subprocess.check_output(I)
+            print(" ".join(I))
+            print("Running the simulator")
+            out = subprocess.check_output(I).decode()
             out = out.split(" ")
-            print "Finished"
+            print("Finished")
+            print(out)
             MT += float(out[13])
         MT /= TRAJECTORIES
         Y = np.append(Y, [[MT]], axis=0)
-        ci = open("X"+l2f(jargs),"wb")
-        co = open("Y"+l2f(jargs),"wb")
-        pickle.dump(X,ci)
-        pickle.dump(Y,co)
+        ci = open("X" + l2f(jargs), "wb")
+        co = open("Y" + l2f(jargs), "wb")
+        pickle.dump(X, ci)
+        pickle.dump(Y, co)
         ci.close()
         co.close()
 
         R = np.append(R, [[MT]], axis=0)
-    print "Returned"
+    print("Returned")
     return R
-        
-dom =[{'name': 'PatientCPRA', 'type': 'continuous', 'domain': (0,1)}]
+
+
+dom = [{'name': 'PatientCPRA', 'type': 'continuous', 'domain': (0, 1)}]
 for BTP in range(4):
+    print("done {}".format(BTP))
     for BTD in range(4):
-        maxi = 20
-        X = np.empty([0,1])
-        Y = np.empty([0,1])
-        jargs = [BTP,BTD,0,0]
-        if jargs == [1,3,0,0]:
-            TRAJECTORIES = 64
-        if jargs == [2,1,0,0]:
-            TRAJECTORIES = 128
-        elif jargs == [3,3,0,0]:
-            TRAJECTORIES = 128
+        print("intermediate {}".format(BTD))
+        maxi = 5
+        X = np.empty([0, 1])
+        Y = np.empty([0, 1])
+        jargs = [BTP, BTD, 0, 0]
+        if jargs == [1, 3, 0, 0]:
+            TRAJECTORIES = 1
+        if jargs == [2, 1, 0, 0]:
+            TRAJECTORIES = 1
+        elif jargs == [3, 3, 0, 0]:
+            TRAJECTORIES = 1
         else:
-            TRAJECTORIES = 32
-        print "Bayesian optimization for master features "+str(jargs)
-        myBopt = BayesianOptimization(f=f, domain=dom, acquisition_type='LCB', num_cores=8)
-        myBopt.run_optimization(max_iter=maxi, eps=0, evaluations_file="E"+l2f(jargs)+".txt", models_file="M"+l2f(jargs)+".txt")
-        myBopt.plot_acquisition("Plot"+l2f(jargs)+".png")
-
-
-
-
+            TRAJECTORIES = 1
+        print("Bayesian optimization for master features " + str(jargs))
+        myBopt = BayesianOptimization(
+            f=f, domain=dom, acquisition_type='LCB', num_cores=8)
+        myBopt.run_optimization(
+            max_iter=maxi,
+            eps=0,
+            evaluations_file="E" + l2f(jargs) + ".txt",
+            models_file="M" + l2f(jargs) + ".txt")
+        myBopt.plot_acquisition("Plot" + l2f(jargs) + ".png")
